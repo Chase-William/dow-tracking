44841ac4f0dc60300cb90c501c70837cf21f6a12
cclcr21 <cclcr21@rit.edu>
2022-06-15 06:16:01 -0400

    + functional qr w/ nextui

---
+++ comps/Scanner.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import dynamic from "next/dynamic";
+const QrReader = dynamic(() => import('react-qr-scanner'), {
+  ssr: false
+  })
+// import QrReader from 'react-qr-scanner'
+
+export default class Test extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 100,
+      result: 'No result',
+    }
+
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(data){
+    if (!!data) {
+      this.setState({
+        result: data.text,
+      })
+    }   
+  }
+  handleError(err){
+    console.error(err)
+  }
+  render(){
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    }
+
+    return(
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
