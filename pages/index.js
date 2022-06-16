ea6949f25b84edeb2719bb41d0f96695985603e4
cclcr21 <cclcr21@rit.edu>
2022-06-16 05:44:26 -0400

    moved script so netlify can build

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,118 @@ import styles from '../styles/Home.module.css'
 import ModalScanner from '../comps/ModalScanner'
 import { Collapse, Grid, Text, Textarea } from '@nextui-org/react';
 import React from 'react'
-import getData from './getData';
 
-const fetcher = (url) => fetch(url).then(res => res.json())
 
 export default function Home() {
   const [materials, setMaterials] = React.useState(new Map())
   const [currentMaterial, setCurrentMaterial] = React.useState(null)
+
+  const getData = () => {
+    return ([
+      {
+        "batchId": "0123456",
+        "materialId": "2523513",
+        "customerId": "5253241",
+        "siteCreated": "3475 E North Union Rd, Bay City, MI 48706",
+        "quantity": 20,
+        "transits": [
+          {
+            "transitId": "352351",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          },
+          {
+            "transitId": "235235",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          },
+          {
+            "transitId": "325325",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          }
+        ],
+        "sustainabilityMetrics": {
+          "carbonEmission": 30,
+          "electricityUsed": 20,
+          "waterUsed": 10,
+          "waste": [
+            "Something",
+            "Something Else",
+            "Something More"
+          ]
+        },
+        "rawMaterials": [
+          "Something",
+          "Something Else",
+          "Something More"
+        ]
+      },
+      {
+        "batchId": "6543210",
+        "materialId": "2523513",
+        "customerId": "5253241",
+        "siteCreated": "3475 E North Union Rd, Bay City, MI 48706",
+        "quantity": 20,
+        "transits": [
+          {
+            "transitId": "352351",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          },
+          {
+            "transitId": "235235",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          },
+          {
+            "transitId": "325325",
+            "dateTimeDeparture": "10:45:00 10/08/2022",
+            "dateTimeReceived": "12:45:00 10/08/2022",
+            "deliveredFrom": "3475 E North Union Rd, Bay City, MI 48706",
+            "deliveredTo": "3475 E North Union Rd, Bay City, MI 48706",
+            "transitType": "Train",
+            "arrived": true
+          }
+        ],
+        "sustainabilityMetrics": {
+          "carbonEmission": 30,
+          "electricityUsed": 20,
+          "waterUsed": 10,
+          "waste": [
+            "Something",
+            "Something Else",
+            "Something More"
+          ]
+        },
+        "rawMaterials": [
+          "Something",
+          "Something Else",
+          "Something More"
+        ]
+      }
+    ])
+  }
 
   const handleMaterialScanned = (text) => {
     if (!materials.has(text)) {
