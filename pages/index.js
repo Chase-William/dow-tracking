9ce6e854d04e21d2d0260c5c329e40f56e0797dd
cclcr21 <cclcr21@rit.edu>
2022-06-17 21:47:54 -0400

    reset to default landing

--- pages/index.js
+++ pages/index.js
@@ -122,8 +122,8 @@ const getData = () => {
 export default function Home() {
   const [materials, setMaterials] = React.useState(new Map())
   // remove default value for anything other than ui testing/dev
-  // const [currentMaterial, setCurrentMaterial] = React.useState(null)
-  const [currentMaterial, setCurrentMaterial] = React.useState(getData()[0])
+  const [currentMaterial, setCurrentMaterial] = React.useState(null)
+  //const [currentMaterial, setCurrentMaterial] = React.useState(getData()[0])
 
   const handleMaterialScanned = (text) => {
     if (!materials.has(text)) {
